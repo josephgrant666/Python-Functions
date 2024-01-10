59e55d0a1c88f48d00f2a38d3731100d3bda0fe0
Joe Grant <128994377+josephgrant666@users.noreply.github.com>
2024-01-10 19:56:26 +0000

    Add files via upload

---
+++ Lists/1. Lists and extracting indexes.py
@@ -0,0 +1,15 @@
+# Lists and extracting indexes 
+
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# Get index of 'germany': ind_ger
+ind_ger = countries.index("germany")
+
+# Use ind_ger to print out capital of Germany
+print(capitals[ind_ger])
+
+# The function therefore was:
+
+list.index('list item')
