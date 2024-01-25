3434fd6035d593fc1866e24a9c420e6f588f83d8
Joe Grant <128994377+josephgrant666@users.noreply.github.com>
2024-01-25 22:04:01 +0000

    Add files via upload

---
+++ Data visualisation/10. Additional customizations, text and grid.py
@@ -0,0 +1,6 @@
+# Additional customizations, text adds text at given co-ordinates. grid() adds a grid onto the graph
+plt.text(1550, 71, 'India')
+plt.text(5700, 80, 'China')
+
+# Add grid() call
+plt.grid(True)
