713feffc1f886428b86da09aa436a1daedb404d9
Joe Grant <128994377+josephgrant666@users.noreply.github.com>
2024-01-30 22:09:23 +0000

    Add files via upload

---
+++ Arrays/1. Comparing arrays.py
@@ -0,0 +1,7 @@
+# Read CSV files with pandas
+
+x = np.array([18.0, 20.0, 10.75, 9.50])
+y = np.array([14.0, 24.0, 14.25, 9.0])
+
+x < y # Produces [ True  True False False]
+		 [False  True  True False]
