1779c0aeb8d1cafa7d2e7d36187f7ccb960fb773
Joe Grant <128994377+josephgrant666@users.noreply.github.com>
2024-02-14 12:01:11 +0000

    Add files via upload

--- Flow Controls Statements/1. if, elif and else.py
+++ Flow Controls Statements/1. if, elif and else.py
@@ -1,1 +1,10 @@
+# if, elif and else
 
+x = 1
+
+if x == 0:		# if x equals 0, then print 'yes'
+	print('Yes')
+elif x == 2
+	print('No')	# if the if statement before is false, then check for this argument. elif can be used multiple times. 
+else :
+	print('Maybe')	# if everything so far is false, then pass this argument
