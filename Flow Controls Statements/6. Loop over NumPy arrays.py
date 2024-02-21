8dc8e83d89e7e89ad9543cc7a59110f31894ff54
Joe Grant <128994377+josephgrant666@users.noreply.github.com>
2024-02-21 22:07:32 +0000

    Add files via upload

---
+++ Flow Controls Statements/6. Loop over NumPy arrays.py
@@ -0,0 +1,9 @@
+# Loop over NumPy arrays
+          
+# For loop over np_height
+for x in np_height :
+    print(str(x) + str(" inches"))
+
+# For loop over np_baseball
+for x in np.nditer(np_baseball) :
+    print(str(x))
