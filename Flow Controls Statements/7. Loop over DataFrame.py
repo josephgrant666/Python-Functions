cdd2e2428d79fb3c65a925b5e0efb40ab2d8d76d
Joe Grant <128994377+josephgrant666@users.noreply.github.com>
2024-02-23 22:47:22 +0000

    Add files via upload

---
+++ Flow Controls Statements/7. Loop over DataFrame.py
@@ -0,0 +1,10 @@
+# Loop over DataFrame
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+         
+# Iterate over rows of cars
+for lab, row in cars.iterrows():
+    print(lab)
+    print(row)
