6bfc5e161d89fb87ef6449069022c63105b601e0
Joe Grant <128994377+josephgrant666@users.noreply.github.com>
2024-02-24 13:40:58 +0000

    Add files via upload

---
+++ Flow Controls Statements/8. Loop that adds a column onto DataFrame .apply() method.py
@@ -0,0 +1,8 @@
+# Loop that adds a column onto DataFrame .apply() method
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Use .apply(str.upper)
+cars["COUNTRY"] = cars["country"].apply(str.upper)
