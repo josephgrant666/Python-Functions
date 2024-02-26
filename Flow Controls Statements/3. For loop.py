0c2657ade31fb82d86463c503f1d6d6036f7373c
Joe Grant <128994377+josephgrant666@users.noreply.github.com>
2024-02-26 17:42:57 +0000

    Add files via upload

---
+++ Flow Controls Statements/3. For loop.py
@@ -0,0 +1,13 @@
+# for loops 
+
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+for measurement in areas : # Assigns variable to each instance of a value in the variable.
+    print(measurement)
+
+# Will return 
+11.25
+18.0
+20.0
+10.75
+9.5
